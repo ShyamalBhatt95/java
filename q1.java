0ad84c8a59d38eb7260f3d997ca6438ec61ef2dc
ShyamalBhatt95 <shyamalbhatt95@gmail.com>
2021-02-18 21:56:48 +0530

    initial commit

---
+++ q1.java
@@ -0,0 +1,22 @@
+import java .io.*;
+import java.util.*;
+class q1
+{
+	public static void main(String args[])
+	{	
+		String name ;
+                 int age;
+               double salary;
+               Scanner sc = new Scanner(System.in); 
+                System.out.println(" enter the name");
+		name = sc.nextLine();
+               System.out.println("Ener Age : ");
+                age = sc.nextInt();
+                System.out.println("Ener Salary : ");
+	        salary = sc.nextDouble();
+                System.out.println("  the name is : "+name);		
+                System.out.println("  the age is : "+age);
+		System.out.println("  the salary is : "+salary);
+
+}
+}                                                                                                                                                                                  
